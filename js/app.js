b2491ea5768a5e707e44d6b33e66e2bc9743078b
samalhaj1 <samalhaj627@gmail.com>
2021-06-21 23:23:10 +0300

    add new file

---
+++ js/app.js
@@ -0,0 +1,83 @@
+"use strict";
+
+
+
+
+    let name = prompt ("What's your name? "); 
+   alert("Welcome "+ name);
+
+   let Age = prompt("How old are you ?");
+  if (Age >18 ) {
+      alert ('Welcome again');
+      console.log("Welcome again");
+}
+
+  else{
+      alert ('I am sorry go search in another place'); 
+      console.log("I am sorry go search in another place");
+  }
+
+
+ let name2 = prompt("Am i syrian?");
+  switch (name2.toLowerCase()) {
+      case 'yes':
+          case 'y':
+          alert('yes, I am syrian  '  + name);
+          break;
+          case 'no':
+            case 'n':
+            alert('no, I am syrian  '  + name );
+            break;
+         }
+
+         let name3 = prompt("do you want to be a web developer?");
+  switch (name3.toLowerCase()) {
+      case 'yes':
+          case 'y':
+          alert('go a head  '  + name);
+          break;
+          case 'no':
+            case 'n':
+            alert('sorry go to another website '  + name );
+            break;
+         }
+
+         let name4 = prompt("do you like programming?");
+  switch (name4.toLowerCase()) {
+      case 'yes':
+          case 'y':
+          alert('welcome again '  + name);
+          break;
+          case 'no':
+            case 'n':
+            alert('i am sad goodbye '  + name );
+            break;
+         }
+
+         let name5 = prompt("did you make a website before?");
+  switch (name5.toLowerCase()) {
+      case 'yes':
+          case 'y':
+          alert('very good go a head  '  + name);
+          break;
+          case 'no':
+            case 'n':
+            alert('good luck then  '  + name );
+            break;
+         }
+
+         let name6 = prompt("are you sure that you want to complete?");
+  switch (name6.toLowerCase()) {
+      case 'yes':
+          case 'y':
+          alert('so welcome to my website '  + name);
+          break;
+          case 'no':
+            case 'n':
+            alert('i am sorry you cant get in to my website '  + name );
+            break;
+         }
+
+         
+  
+    
